8bbbb71c8772e53013839df1678364a2e6c12bfb
JunHyoung <jh061993@gmail.com>
2021-07-26 15:14:22 +0900

    트래킹결과 속도 그래프 그리는 코드

---
+++ PlenOpticVot_SiamRPN++/graph.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = open('save/0725_152641/result.txt', 'r')
+frame_list = []  # 20 : 24.723
+frame_list_index = -1
+while True:
+    line = file.readline()
+    if line.startswith('Select'):  # Done. (1472.480s)
+        break
+
+    if line.startswith('['):
+        line = line.split()
+        if line[1] == 'total':
+            frame_list_index += 1
+            frame_dict = {}
+            frame = int(line[0][1:-1])
+            sec = float(line[-2])
+            frame_dict[frame] = sec
+            frame_list.append(frame_dict)
+
+    # elif line[:-1].endswith('s)'):  # 544x960 Done. (0.459s)
+    #     sec = float(line.split()[-1][1:-2])
+    #     frame_list[frame_list_index][frame][0] += sec
+    #     frame_list[frame_list_index][frame][1] += 1
+        # print(sec)
+
+    # print(line)
+print(frame_list)
+
+
+# graph
+x = []
+y = []
+
+for frame in frame_list:
+    frame, sec = list(frame.items())[0]
+    x.append(frame)
+    y.append(sec)
+
+y_average = sum(y) / len(y)
+y_average = [y_average] * len(y)
+
+plt.plot(x, y, marker="o", label="Frame")
+plt.plot(x, y_average, label="Average")
+
+plt.xlabel("Frame")
+plt.ylabel("SEC")
+plt.title("Frame average in Focal planes by SiamRPN++")
+plt.legend()
+plt.show()
+
+
+
+
+
